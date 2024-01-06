15eeb96a4fadd50729ed1f99a0acac0ff62ef844
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-01-06 03:18:03 -0500

    The beginning

---
+++ assets/js/clickinteractions.js
@@ -0,0 +1,41 @@
+document.getElementById("intro1").onclick = function () {
+    document.getElementById("intro2").classList.add("show");
+    document.getElementById("intro1").classList.remove("clickable");
+    document.getElementById("intro1").classList.add("show");
+}
+
+document.getElementById("intro3").onclick = function () {
+    document.getElementById("name1").classList.add("show");
+    document.getElementById("intro3").classList.remove("clickable");
+    document.getElementById("intro3").classList.add("show");
+}
+
+document.getElementById("name2").onclick = function () {
+    document.getElementById("name3").classList.add("show");
+    document.getElementById("name2").classList.remove("clickable");
+    document.getElementById("name2").classList.add("show");
+}
+
+document.getElementById("woo").onclick = function () {
+    document.getElementById("woo2").classList.add("show");
+    document.getElementById("woo").classList.remove("clickable");
+    document.getElementById("woo").classList.add("show");
+}
+
+document.getElementById("further1").onclick = function () {
+    document.getElementById("anyways").classList.add("show");
+    document.getElementById("further1").classList.remove("clickable");
+    document.getElementById("further1").classList.add("show");
+}
+
+document.getElementById("anyways").onclick = function () {
+    document.getElementById("about1").classList.add("show");
+    document.getElementById("anyways").classList.remove("clickable");
+    document.getElementById("anyways").classList.add("show");
+}
+
+document.getElementById("about1").onclick = function () {
+    document.getElementById("about2").classList.add("show");
+    document.getElementById("about1").classList.remove("clickable");
+    document.getElementById("about1").classList.add("show");
+}
